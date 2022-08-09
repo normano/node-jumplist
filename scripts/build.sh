3bec0fa399b397e2d9843ed92dad4ea911b71858
NormO <normo.pub@gmail.com>
2022-08-08 23:28:19 -0500

    splitting esm and cjs

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,21 @@
 set -e
 set -x
 
-rm -fr ./dist/*
+rm -fr ./dist/
+rm -fr _intermediate/
+
+mkdir -p _intermediate/mjs
+cp -r src/** _intermediate/mjs/
+pnpm dlx renamer --find .ts --replace .mts "_intermediate/mjs/**/*"
+xs_cmd node:replace_import_ext --userPath _intermediate/mjs --inputFileExt mts --inputImportExt js --importExt mjs
+
+mkdir -p _intermediate/cjs
+cp -r src/** _intermediate/cjs/
+pnpm dlx renamer --find .ts --replace .cts "_intermediate/cjs/**/*"
+xs_cmd node:replace_import_ext --userPath _intermediate/cjs --inputFileExt cts --inputImportExt js --importExt cjs
 
 tsc -p tsconfig.mjs.json
-pnpx -y convert-extension mjs dist/node
-tsc
+tsc -p tsconfig.cjs.json
 tsc -p tsconfig.browser.json
+
+rm -fr _intermediate
